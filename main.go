0809714fd1670294dbee39df75ef1ee0fbebf41a
Nitesh Mhatre <n.mhatre90@gmail.com>
2026-03-21 22:38:19 +0530

    fisrt get and post

--- main.go
+++ main.go
@@ -2,14 +2,34 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/nitesh-mhatre/go-rest-api/models"
+	"net/http"
+
 )
 
 func main() {
 	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
+	r.GET("/events", getEvents)
+	r.POST("/events",createEvent)
 	r.Run() // listen and serve on 0.0.0.0:8080
+}
+
+func getEvents(c *gin.Context){
+	events := models.GetAllEvents()
+	c.JSON(http.StatusOK, events)
+
+}
+
+func createEvent(c *gin.Context){
+	var event models.Event
+	if err := c.ShouldBindJSON(&event); err != nil {
+		// fallback to query binding when json/form is absent
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	event.Save()
+
+	c.JSON(http.StatusCreated, event)
+
 }
---
+++ models/events.go
@@ -0,0 +1,23 @@
+package models
+
+
+type Event struct {
+	ID int 
+	Name string `binding:"required"`
+	Description string `binding:"required"`
+	Location string `binding:"required"`
+	DateTime string `binding:"required"`
+	UserID int
+}
+
+var events []Event = []Event{
+
+}
+
+func (e Event) Save() {
+	events = append(events, e)
+}
+
+func GetAllEvents() []Event {
+	return events
+}
